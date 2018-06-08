69446be19bd0ac5d06bc3feee49661f3398ddd93
Katie Shin <kshin@dhcp-10-105-145-127.wireless.northwestern.private>
2018-06-08 16:40:34 -0500

    added predicates for checking if player has won, and if board is full

--- src/net/connect4/Connect4Logic.pl
+++ src/net/connect4/Connect4Logic.pl
@@ -2,6 +2,7 @@
 name("Katie", "Shin", "ksw092").
 name("Joshua", "Smith", "jas790").
 
+:- use_module(library(clpfd)).
 
 % addPieceToColumn(+Column, +Player, -OutputColumn)
 addPieceToColumn([], _, []).
@@ -15,6 +16,27 @@ addPiece([C|Board], CNum, Player, [C|Result]) :- \+CNum=0, D is CNum-1, addPiece
 
 
 % canWin(+Board, ?ColumnNumber)
+sublist(Sublist, List) :- append([_, Sublist, _], List).
+elementOfRow(Row, Index, E) :- nth0(Index, Row, E).
+extract_element(L, L1, [H|L1]):- length(L1, N1), length(L2, N1), 
+               					 append(L2, [H|_], L).
+diagonal1(In, Out):- foldl(extract_element, In, [], Res), 
+                	 reverse(Res,Out).
+diagonal2(In, Out):- reverse(In, In2),
+                	 foldl(extract_element, In2, [], Out).
+checkFullBoard(Board) :- foreach(member(C, Board),
+								 \+member(0, C)).
+
+
+
+checkVertical(Board, CNum, Player) :- nth0(CNum, Board, C),
+									  sublist([Player, Player, Player, Player], C).
+checkHorizontal(Board, RNum, Player) :- transpose(Board, M), nth0(RNum, M, R),
+										sublist([Player, Player, Player, Player], R).
+checkDiagonal(Board, Player) :- diagonal1(Board, D),
+								sublist([Player, Player, Player, Player], D).
+checkDiagonal(Board, Player) :- diagonal2(Board, D),
+								sublist([Player, Player, Player, Player], D).
 
 
 % canForceWin(+Board, ?ColumnNumber)
