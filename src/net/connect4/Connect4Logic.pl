1d4a9b6207c248679caf93f644cd40db4017555c
Katie Shin <kshin@dhcp-10-105-145-127.wireless.northwestern.private>
2018-06-10 17:54:37 -0500

    fixed checkDiagonal, removed unnecessary Index argument for checkWin

--- src/net/connect4/Connect4Logic.pl
+++ src/net/connect4/Connect4Logic.pl
@@ -5,12 +5,7 @@ name("Joshua", "Smith", "jas790").
 :- use_module(library(clpfd)).
 
 
-% Player List
-playerList([a, b]).
-otherPlayer(Player, Opponent) :-
-	playerList(List),
-	member(Opponent, List),
-	Opponent \= Player.
+% hard-coded values
 index(0).
 index(1).
 index(2).
@@ -18,6 +13,30 @@ index(3).
 index(4).
 index(5).
 index(6).
+
+diag1Col(0).
+diag1Col(1).
+diag1Col(2).
+diag1Col(3).
+diag1Row(0).
+diag1Row(1).
+diag1Row(2).
+
+diag2Col(3).
+diag2Col(4).
+diag2Col(5).
+diag2Col(6).
+diag2Row(0).
+diag2Row(1).
+diag2Row(2).
+
+
+% Player List
+playerList([a, b]).
+otherPlayer(Player, Opponent) :-
+	playerList(List),
+	member(Opponent, List),
+	Opponent \= Player.
 
 
 % addPieceToColumn(+Column, +Player, -OutputColumn)
@@ -41,17 +60,33 @@ addPiece([C|Board], CNum, Player, [C|Result]) :-
 % predicates for checking if any player has won
 sublist(Sublist, List) :-
 	append([_, Sublist, _], List).
-elementOfRow(Row, Index, E) :-
-	nth0(Index, Row, E).
-extract_element(L, L1, [H|L1]):-
-	length(L1, N1), length(L2, N1),
-	append(L2, [H|_], L).
-diagonal1(In, Out):-
-	foldl(extract_element, In, [], Res),
-	reverse(Res,Out).
-diagonal2(In, Out):-
-	reverse(In, In2),
-	foldl(extract_element, In2, [], Out).
+
+diag1(Board, Player) :-
+	diag1Col(CNum1), diag1Row(RNum1),
+	CNum2 is CNum1 + 1, CNum3 is CNum1 + 2, CNum4 is CNum1 + 3,
+	RNum2 is RNum1 + 1, RNum3 is RNum1 + 2, RNum4 is RNum1 + 3,
+	nth0(CNum1, Board, Col1),
+	nth0(CNum2, Board, Col2),
+	nth0(CNum3, Board, Col3),
+	nth0(CNum4, Board, Col4),
+	nth0(RNum1, Col1, Player),
+	nth0(RNum2, Col2, Player),
+	nth0(RNum3, Col3, Player),
+	nth0(RNum4, Col4, Player).
+
+diag2(Board, Player) :-
+	diag2Col(CNum1), diag2Row(RNum1),
+	CNum2 is CNum1 - 1, CNum3 is CNum1 - 2, CNum4 is CNum1 - 3,
+	RNum2 is RNum1 + 1, RNum3 is RNum1 + 2, RNum4 is RNum1 + 3,
+	nth0(CNum1, Board, Col1),
+	nth0(CNum2, Board, Col2),
+	nth0(CNum3, Board, Col3),
+	nth0(CNum4, Board, Col4),
+	nth0(RNum1, Col1, Player),
+	nth0(RNum2, Col2, Player),
+	nth0(RNum3, Col3, Player),
+	nth0(RNum4, Col4, Player).
+
 isBoardFull(Board) :-
 	foreach(member(C, Board), \+member(0, C)).
 
@@ -63,34 +98,29 @@ checkHorizontal(Board, RNum, Player) :-
 	transpose(Board, M), nth0(RNum, M, R),
 	sublist([Player, Player, Player, Player], R).
 checkDiagonal(Board, Player) :-
-	diagonal1(Board, D),
-	sublist([Player, Player, Player, Player], D).
-checkDiagonal(Board, Player) :-
-	diagonal2(Board, D),
-	sublist([Player, Player, Player, Player], D).
+	diag1(Board, Player) ;
+	diag2(Board, Player).
 
 
 % Index should start at 0 to check all columns and rows
-checkWin(Board, Index, Player) :-
-	checkVertical(Board, Index, Player) ;
+checkWin(Board, Player) :-
+	index(Index),
+	(checkVertical(Board, Index, Player) ;
 	checkHorizontal(Board, Index, Player) ;
-	checkDiagonal(Board, Player).
-checkWin(Board, Index, Player) :-
-	Index2 is Index + 1, Index2 < 7,
-	checkWin(Board, Index2, Player).
+	checkDiagonal(Board, Player)).
 
 opponentWin(Board, Player) :-
 	otherPlayer(Player, Opponent),
-	checkWin(Board, 0, Opponent).
+	checkWin(Board, Opponent).
 
 
 % canForceWin(+Board, ?ColumnNumber)
 canForceWin(Board, CNum, Player) :-
-	checkWin(Board, 0, Player).
+	checkWin(Board, Player).
 canForceWin(Board, CNum, Player) :-
 	\+isBoardFull(Board),
 	addPiece(Board, CNum, Player, ResultBoard),
-	checkWin(ResultBoard, 0, Player).
+	checkWin(ResultBoard, Player).
 canForceWin(Board, CNum, Player) :-
 	\+isBoardFull(Board),
 	addPiece(Board, CNum, Player, ResultBoard),
