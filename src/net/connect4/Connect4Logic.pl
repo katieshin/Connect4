4111445ce1882d9ed262d983c2a3decf9ec1dfb8
Joshua Smith <joshuasmith2019@u.northwestern.edu>
2018-06-10 21:03:11 -0500

    Fixed the main minmax algorithm in the prolog code.

--- src/net/connect4/Connect4Logic.pl
+++ src/net/connect4/Connect4Logic.pl
@@ -4,40 +4,127 @@ name("Joshua", "Smith", "jas790").
 
 :- use_module(library(clpfd)).
 
+
+% hard-coded values
+index(3).
+index(2).
+index(4).
+index(1).
+index(5).
+index(0).
+index(6).
+
+diag1Col(0).
+diag1Col(1).
+diag1Col(2).
+diag1Col(3).
+diag1Row(0).
+diag1Row(1).
+diag1Row(2).
+
+diag2Col(3).
+diag2Col(4).
+diag2Col(5).
+diag2Col(6).
+diag2Row(0).
+diag2Row(1).
+diag2Row(2).
+
+
+% Player List
+playerList(["*", "#"]).
+otherPlayer(Player, Opponent) :-
+	playerList(List),
+	member(Opponent, List),
+	Opponent \= Player.
+
+
 % addPieceToColumn(+Column, +Player, -OutputColumn)
-addPieceToColumn([], _, []).
-addPieceToColumn([E|C], Player, [E2|OC]) :- E = 0, E2 = Player, C=OC.
-addPieceToColumn([E|C], Player, [E2|OC]) :- E = E2, \+E=0, addPieceToColumn(C, Player, OC).
+addPieceToColumn([E|C], Player, [E2|OC]) :-
+	E = 0, E2 = Player, C=OC.
+addPieceToColumn([E|C], Player, [E2|OC]) :-
+	E = E2,
+	\+E=0, addPieceToColumn(C, Player, OC).
 
 
 % addPiece(+InputBoard, +ColumnNumber, +Player, -ResultBoard)
-addPiece([C|Rest], 0, Player, [D|Rest]) :- addPieceToColumn(C, Player, D).
-addPiece([C|Board], CNum, Player, [C|Result]) :- \+CNum=0, D is CNum-1, addPiece(Board, D, Player, Result).
+addPiece([C|Rest], 0, Player, [D|Rest]) :-
+	addPieceToColumn(C, Player, D).
+addPiece([C|Board], CNum, Player, [C|Result]) :-
+	\+CNum=0,
+	D is CNum-1,
+	addPiece(Board, D, Player, Result).
 
 
-% canWin(+Board, ?ColumnNumber)
-sublist(Sublist, List) :- append([_, Sublist, _], List).
-elementOfRow(Row, Index, E) :- nth0(Index, Row, E).
-extract_element(L, L1, [H|L1]):- length(L1, N1), length(L2, N1), 
-               					 append(L2, [H|_], L).
-diagonal1(In, Out):- foldl(extract_element, In, [], Res), 
-                	 reverse(Res,Out).
-diagonal2(In, Out):- reverse(In, In2),
-                	 foldl(extract_element, In2, [], Out).
-checkFullBoard(Board) :- foreach(member(C, Board),
-								 \+member(0, C)).
+% predicates for checking if any player has won
+sublist(Sublist, List) :-
+	append([_, Sublist, _], List).
+
+diag1(Board, Player) :-
+	diag1Col(CNum1), diag1Row(RNum1),
+	CNum2 is CNum1 + 1, CNum3 is CNum1 + 2, CNum4 is CNum1 + 3,
+	RNum2 is RNum1 + 1, RNum3 is RNum1 + 2, RNum4 is RNum1 + 3,
+	nth0(CNum1, Board, Col1),
+	nth0(CNum2, Board, Col2),
+	nth0(CNum3, Board, Col3),
+	nth0(CNum4, Board, Col4),
+	nth0(RNum1, Col1, Player),
+	nth0(RNum2, Col2, Player),
+	nth0(RNum3, Col3, Player),
+	nth0(RNum4, Col4, Player).
+
+diag2(Board, Player) :-
+	diag2Col(CNum1), diag2Row(RNum1),
+	CNum2 is CNum1 - 1, CNum3 is CNum1 - 2, CNum4 is CNum1 - 3,
+	RNum2 is RNum1 + 1, RNum3 is RNum1 + 2, RNum4 is RNum1 + 3,
+	nth0(CNum1, Board, Col1),
+	nth0(CNum2, Board, Col2),
+	nth0(CNum3, Board, Col3),
+	nth0(CNum4, Board, Col4),
+	nth0(RNum1, Col1, Player),
+	nth0(RNum2, Col2, Player),
+	nth0(RNum3, Col3, Player),
+	nth0(RNum4, Col4, Player).
+
+isBoardFull(Board) :-
+	foreach(member(C, Board), \+member(0, C)).
 
 
+checkVertical(Board, CNum, Player) :-
+	nth0(CNum, Board, C),
+	sublist([Player, Player, Player, Player], C).
+checkHorizontal(Board, RNum, Player) :-
+	transpose(Board, M), nth0(RNum, M, R),
+	sublist([Player, Player, Player, Player], R).
+checkDiagonal(Board, Player) :-
+	diag1(Board, Player) ;
+	diag2(Board, Player).
 
-checkVertical(Board, CNum, Player) :- nth0(CNum, Board, C),
-									  sublist([Player, Player, Player, Player], C).
-checkHorizontal(Board, RNum, Player) :- transpose(Board, M), nth0(RNum, M, R),
-										sublist([Player, Player, Player, Player], R).
-checkDiagonal(Board, Player) :- diagonal1(Board, D),
-								sublist([Player, Player, Player, Player], D).
-checkDiagonal(Board, Player) :- diagonal2(Board, D),
-								sublist([Player, Player, Player, Player], D).
+
+% Index should start at 0 to check all columns and rows
+checkWin(Board, Player) :-
+	index(Index),
+	(checkVertical(Board, Index, Player) ;
+	checkHorizontal(Board, Index, Player) ;
+	checkDiagonal(Board, Player)).
+
+opponentWin(Board, Player) :-
+	otherPlayer(Player, Opponent),
+	checkWin(Board, Opponent).
 
 
 % canForceWin(+Board, ?ColumnNumber)
+canForceWin(Board, CNum, Player) :-
+	index(CNum),
+	addPiece(Board, CNum, Player, ResultBoard),
+	checkWin(ResultBoard, Player).
+canForceWin(Board, CNum, Player) :-
+	format("~w\n", [Board]),	
+	index(CNum),
+	addPiece(Board, CNum, Player, ResultBoard),
+	forall(index(CNum2), helper(ResultBoard, CNum2, Player)).
+
+helper(ResultBoard, CNum2, Player) :-	
+	otherPlayer(Player, Opponent), addPiece(ResultBoard, CNum2, Opponent, RB2), \+ checkWin(RB2, Opponent), canForceWin(RB2, _, Player).
+
 
